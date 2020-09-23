8ea659a7491a06cd907f51e3d135b289689dace4
Crowdbotics <team@crowdbotics.com>
2020-09-23 18:29:37 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/adailyhelperdiagram_20609/MainActivity.java
@@ -0,0 +1,15 @@
+package com.adailyhelperdiagram_20609;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "adailyhelperdiagram_20609";
+    }
+}
